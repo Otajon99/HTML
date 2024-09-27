15b202ca5d5fa0cfb9496c71f837bb6a37755d53
Otajon99 <otajonburxonov99@gmail.com>
2024-09-27 15:40:04 -0400

    Pushing my new exercises

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,33 @@
+const todoList=['make dinner','wash dishes'];
+
+renderTodoList();
+
+
+function renderTodoList(){
+   let todoListHTML='';
+   for(let i=0;i<todoList.length;i++){
+      const todo=todoList[i];
+      const html=`<p>${todo}</p>`;
+      todoListHTML+=html;  
+   }
+   console.log(todoListHTML);
+   
+   document.querySelector('.js-todo-List')
+   .innerHTML = todoListHTML;
+}
+
+
+
+
+
+function addTodo(){
+   const inputElement= document.querySelector('.js-name-input');
+   const name=inputElement.value;
+
+   todoList.push(name);
+   console.log(todoList);
+
+   inputElement.value='';
+
+   renderTodoList();
+}
